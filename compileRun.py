6cc52088fc2030f809cee9b645bbe4d5354b8b7f
Derek Laker <dplaker@mtu.edu>
2016-12-10 19:14:33 +0000

    redid the finding methods

--- compileRun.py
+++ compileRun.py
@@ -5,18 +5,31 @@ class main():
 
 	def getFile(self):
 
-		for file in glob.glob("*.c"):
-			filename = file
-		if (len(filename)) < 1:
-			filename = glob.glob("*.py")
-			return filename
+		if (glob.glob("*.c") != []):
+			return [x for x in glob.glob("*.c")]
+		else:
+			pass
+
+		if (glob.glob("*.py") != []):
+			return [x for x in glob.glob("*.py")]
+		else:
+			pass
 		
 	def enterCommand(self, filename):
 
 		os.system("clear")
-		filename = "".join(filename)
-		newfilename = filename.strip('.c')
-		os.system("gcc -o %s %s" %(newfilename, filename))
+		filename = "".join(filename[0])
+
+		if (filename[-2:] == ".c"):
+			newfilename = filename.strip('.c')
+			os.system("gcc -o %s %s" %(newfilename, filename))
+		else:
+			pass
+
+		if (filename[-3:] == ".py"):
+			os.system("python %s" %(filename))
+		else:
+			pass
 			
 if __name__ == '__main__':
 	filename = main().getFile()
